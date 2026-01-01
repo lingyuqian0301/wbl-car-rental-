293fb4e92a9f2670df3712736d2b942e3b89ee7a
Irfan Azahan <112186156+IrfanAzahan@users.noreply.github.com>
2026-01-01 16:31:03 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
 
         <!-- Scripts -->
         <script src="https://cdn.tailwindcss.com"></script>
+
+        <!-- Vite Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
