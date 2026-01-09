79a9f97fb189ee043d8dacb5f35fc2ec65afdb5f
Hafiz <hafiz@example.com>
2026-01-09 14:23:56 +0800

    minor

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $table = 'users';
+    protected $table = 'user';
     protected $primaryKey = 'userID'; 
     public $incrementing = true;
     protected $keyType = 'int';
